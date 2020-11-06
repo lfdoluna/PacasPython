a4b694c0f41e9090e91d0366a9fd34fbacf43e65
lfdoluna <69222086+lfdoluna@users.noreply.github.com>
2020-11-06 11:35:53 -0600

    Eliminando codigo basura
    eliminando scripts basura y creando un metodo de impresion de tickets de forma masiva

--- imprimepacas22.py
+++ imprimepacas22.py
@@ -31,18 +31,34 @@ def ConsultaDB(consulta):
 				return resultado
 			except:
 				pass
-for i in range(6989, 7061, 1):
+
+def entrada():
+    global r_i
+    global r_o
+    try:
+        r_i = int(input("Desde que folio empezara: "))
+        r_o = int(input("Hasta que folio terminara: "))
+    except Exception as e:
+        print e
+        print "Error, por favor ingrese de nuevo los datos"
+        entrada()
+        
+global r_i
+global r_o
+print "Script de apoyo para la impresion masiva de tickets"
+entrada()
+for i in range(r_i, r_o, 1):
     sqlquery = "SELECT fecha, kilogramos, tipo_paca FROM pacas WHERE id_paca = {};".format(i)
     data = ConsultaDB(sqlquery)
-    #p = subprocess.Popen('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://192.168.5.243/merma/__extensions/tcpdf/examples/ticket.paca.php?fecha={}&peso={}&tipopaca={}&codigo={}"'.format(data[0],data[1],data[2],i), 
-	                                #stdout=subprocess.PIPE, 
-	                                #shell=True)
+    # Descarga el ticket
+    p = subprocess.Popen('C:\PacasPython\wget.exe -O C:\PacasPython\Ticket_de_Pacas{}.pdf -c "http://192.168.5.243/merma/__extensions/tcpdf/examples/ticket.paca.php?fecha={}&peso={}&tipopaca={}&codigo={}"'.format(i,data[0],data[1],data[2],i), 
+	                                stdout=subprocess.PIPE, 
+	                                shell=True)
     print 'Llamando a printerPacas....'
     time.sleep(5)
     # Imprime el tiket
-    fileP = 'C:\Users\BI\Downloads\Ticket_de_Pacas{}.pdf'.format(i)
-    p = subprocess.Popen('"C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"  /n /s /h /t "{}" "QL-800_pacas"'.format(fileP), stdout=subprocess.PIPE, shell=True)
+    fileP = 'C:\PacasPython\Ticket_de_Pacas{}.pdf'.format(i)
+    p = subprocess.Popen('"C:\Program Files\Adobe\Reader 11.0\Reader\AcroRd32.exe"  /n /s /h /t "{}" "Brother QL-800"'.format(fileP), stdout=subprocess.PIPE, shell=True)
     print '**********************----'
     print (fileP)
     print '**********************----'
-    #subprocess.call('python c:\PacasPython\printerPacas.py')
